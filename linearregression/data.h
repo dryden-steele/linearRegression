e150474d1ba9a1beafa5013420f57eb3eff35572
dryden-steele <31872522+dryden-steele@users.noreply.github.com>
2018-11-28 16:49:33 -0600

    Add files via upload

--- linearregression/data.h
+++ linearregression/data.h
@@ -3,3 +3,16 @@
 // FUNCTION PROTOTYPES
 //--------------------------------------------------------
 bool populatedata(double independent[8], double dependent[8], int dataset);
+bool populatedataJan(double independent[8], double dependent[8], int dataset);
+bool populatedataFeb(double independent[8], double dependent[8], int dataset);
+bool populatedataMar(double independent[8], double dependent[8], int dataset);
+bool populatedataApr(double independent[8], double dependent[8], int dataset);
+bool populatedataMay(double independent[8], double dependent[8], int dataset);
+bool populatedataJun(double independent[8], double dependent[8], int dataset);
+bool populatedataJul(double independent[8], double dependent[8], int dataset);
+bool populatedataAug(double independent[8], double dependent[8], int dataset);
+bool populatedataOct(double independent[8], double dependent[8], int dataset);
+bool populatedataSep(double independent[8], double dependent[8], int dataset);
+bool populatedataNov(double independent[8], double dependent[8], int dataset);
+bool populatedataDec(double independent[8], double dependent[8], int dataset);
+
--- linearregression/main.c
+++ linearregression/main.c
@@ -24,9 +24,13 @@ int main(void)
     //temperatures
     double dependent[8];
 
-    for(int dataset = 1; dataset <=3; dataset++)
+
+
+
+    //one of these for loops for each month    
+    for(int dataset = 1; dataset <=5; dataset++)
     {
-        if(populatedata(independent, dependent, dataset))
+        if(populatedataJan(independent, dependent, dataset))//populateDataJan
         {
             printf("Data Set %d\n-----------\n", dataset);
 
@@ -39,6 +43,182 @@ int main(void)
             printf("y = %gx + %g\n\n", lr.a, lr.b);
         }
     }
+
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataFeb(independent, dependent, dataset))//populateDataFeb
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataMar(independent, dependent, dataset))//populateDataMar
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataApr(independent, dependent, dataset))//populateDataApr
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataMay(independent, dependent, dataset))//populateDataMay
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataJun(independent, dependent, dataset))//populateDataJun
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataJul(independent, dependent, dataset))//populateDataJul
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataAug(independent, dependent, dataset))//populateDataAug
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataOct(independent, dependent, dataset))//populateDataOct
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataSept(independent, dependent, dataset))//populateDataSept
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataNov(independent, dependent, dataset))//populateDataNov
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+
+for(int dataset = 1; dataset <=5; dataset++)
+    {
+        if(populatedataDec(independent, dependent, dataset))//populateDataDec
+        {
+            printf("Data Set %d\n-----------\n", dataset);
+
+            printdata(independent, dependent, 8);
+
+            lin_reg lr;
+
+            linear_regression(independent, dependent, 8, &lr);
+
+            printf("y = %gx + %g\n\n", lr.a, lr.b);
+        }
+    }
+
+
+
+
+
+
+
 
     return EXIT_SUCCESS;
 }
