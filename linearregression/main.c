5920ebc5e5563cf83695e1e99e21eb5d1bc163ac
Lameter <djlamet@oak.ad.ilstu.edu>
2018-12-02 13:20:16 -0600

    Added code to use open mpi in the data stages of the application

--- linearregression/main.c
+++ linearregression/main.c
@@ -22,14 +22,17 @@ double predictTemp(double* coefficients,double f);
 
 int main(int argc, char* argv[])
 {
-    int n;
+    int num_processes;
     int rank;
-    int local_csv_num;
-    int local_n;
+    int local_size;
+    int local_output_array_size;
     int* data_sizes;
     int* data_displacements;
-    long int *temp_maxes;
-    long int *temp_mins;
+    int* data_output_array_sizes;
+    int* data_output_array_displacements;
+    int* local_csv_lines;
+    long int* temp_maxes;
+    long int* temp_mins;
     long int* local_temp_maxes;
     long int* local_temp_mins;
     double* t_x;
@@ -38,16 +41,46 @@ int main(int argc, char* argv[])
     double* local_t_y;
     long long int temp_max=0;
     long long int temp_min=0;
+    long int local_temp_max=0;
+    long int local_temp_min=0;
 
     omp_set_num_threads(atoi(argv[1]));
     MPI_Init(&argc,&argv);
-    MPI_Comm_size (MPI_COMM_WORLD, &n);
+    MPI_Comm_size (MPI_COMM_WORLD, &num_processes);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 
     float starttime=MPI_Wtime();
     struct weather_data* local_weather_array;
     struct weather_data* weather_array[CSV_NUM];
     
+    // calculate sizes and displacements for following data
+    data_sizes = malloc(sizeof(int) * num_processes);
+    data_displacements = malloc(sizeof(int) * num_processes);
+
+    printf("CSV_NUM = %d on processes with rank %d\n");
+    local_size = CSV_NUM / num_processes;
+    int remainder = CSV_NUM % num_processes;
+
+    // Build size and displacement arrays
+    for (int i = 0; i < num_processes; i++) {
+        // Start with base size
+        data_sizes[i] = local_size;
+
+        // Account for first displacement
+        if (i == 0) {
+            data_displacements[i] = 0;
+        }
+        else {
+            data_displacements[i] = data_displacements[i - 1] + data_sizes[i - 1];
+        }
+
+        // Add remainder until it is all accounted for
+        if (remainder > 0) {
+            data_sizes[i]++;
+            remainder--;
+        }
+    }
+
     if (rank == 0) {
         for (int i=0;i<CSV_NUM;i++)
         {
@@ -79,7 +112,7 @@ int main(int argc, char* argv[])
             temp_min += temp_mins[i];
         }
 
-        printf("temp_max = %ld\ttemp_min = %ld\nsum = %ld\n", temp_max, temp_min, temp_max + temp_min);
+        printf("temp_max = %lld\ttemp_min = %lld\nsum = %lld\n", temp_max, temp_min, temp_max + temp_min);
         t_x = (double*) malloc(sizeof(double) * (temp_max + temp_min));
         if (t_x == NULL) {
             printf("Not enough memory for x\n");
@@ -90,34 +123,69 @@ int main(int argc, char* argv[])
             printf("Not enough memory for y\n");
             return 1;
         }
+
+        // Build output data sizes and displacements
+        data_output_array_sizes = malloc(sizeof(int) * num_processes);
+        data_output_array_displacements = malloc(sizeof(long int) * num_processes);
+        for (int i = 0; i < num_processes; i++) {
+            data_output_array_sizes[i] = 0;
+
+            for (int j = 0; j < data_sizes[i]; j++) {
+                data_output_array_sizes[i] += temp_maxes[j + data_displacements[i]] + temp_mins[j + data_displacements[i]];
+            }
+            
+            if (i == 0) {
+                data_output_array_displacements[i] = 0;
+            }
+            else {
+                data_output_array_displacements[i] = data_output_array_displacements[i - 1] + data_output_array_sizes[i - 1];
+            }
+        }
     }
 
-
-    // calculate sizes and displacements for following data
+    // Build receiving arrays
+    local_weather_array = malloc(sizeof(struct weather_data) * data_sizes[rank]);
+    //local_temp_maxes = malloc(sizeof(long int) * data_sizes[rank]);
+    //local_temp_mins = malloc(sizeof(long int) * data_sizes[rank]);
+    local_csv_lines = malloc(sizeof(int) * data_sizes[rank]);
 
     // scatter weather array
+    MPI_Scatterv(&weather_array, data_sizes, data_displacements, mpi_weather_data, &local_weather_array, data_sizes[rank], mpi_weather_data, 0, MPI_COMM_WORLD);
+
     // scatter temp maxes
+    //MPI_Scatterv(&temp_maxes, data_sizes, data_displacements, MPI_LONG, &local_temp_maxes, data_sizes[rank], MPI_LONG, 0, MPI_COMM_WORLD);
+
     // scatter temp mins
+    //MPI_Scatterv(&temp_mins, data_sizes, data_displacements, MPI_LONG, &local_temp_mins, data_sizes[rank], MPI_LONG, 0, MPI_COMM_WORLD);
+
+    // scatter output array sizes
+    MPI_Scatter(&data_output_array_sizes, 1, MPI_INT, &local_output_array_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // scatter csv lines
+    MPI_Scatterv(&csv_lines, data_sizes, data_displacements, MPI_INT, &local_csv_lines, data_sizes[rank], MPI_INT, 0, MPI_COMM_WORLD);
+
+    local_t_x = malloc(sizeof(double) * local_output_array_size);
+    local_t_y = malloc(sizeof(double) * local_output_array_size);
 
     long long int count_x = 0;
-    for (long int k=0;k<CSV_NUM;k++)
+    for (long int k=0;k<data_sizes[rank];k++)
     {
         long long int count_max = 0;
         long long int count_min = 0;
-        struct weather_data *tmp=weather_array[k];
-        for (long long int i=0;i<csv_lines[k];i++)
+        struct weather_data *tmp = local_weather_array[k];
+        for (long long int i=0;i<local_csv_lines[k];i++)
         {
             if (strcmp(tmp[i].name,"TMAX") == 0)
             {
-                t_x[count_x] = (double)count_max;
-                t_y[count_x] = (double)tmp[i].value;
+                local_t_x[count_x] = (double)count_max;
+                local_t_y[count_x] = (double)tmp[i].value;
                 count_max++;
                 count_x++;
             }
             else if (strcmp(tmp[i].name,"TMIN") == 0)
             {
-                t_x[count_x] = (double)count_min;
-                t_y[count_x] = (double)tmp[i].value;
+                local_t_x[count_x] = (double)count_min;
+                local_t_y[count_x] = (double)tmp[i].value;
                 count_min++;
                 count_x++;
             }
@@ -125,33 +193,40 @@ int main(int argc, char* argv[])
     }
 
     // gather local t x
+    MPI_Gatherv(&local_t_x, local_output_array_size, MPI_DOUBLE, &t_x, data_output_array_sizes, data_output_array_displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
     // gather local t y
+    MPI_Gatherv(&local_t_y, local_output_array_size, MPI_DOUBLE, &t_y, data_output_array_sizes, data_output_array_displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    //prints all data points 
-    //for (int i=0;i<temp_max+temp_min;i++)
-    //{
-    //    printf("(%.3f, %.3f)\n",t_y[i],t_x[i]);
-    //}
+    // Do prediction
+    if (rank == 0) {
+        //prints all data points 
+        //for (int i=0;i<temp_max+temp_min;i++)
+        //{
+        //    printf("(%.3f, %.3f)\n",t_y[i],t_x[i]);
+        //}
 
-    // Stores the coefficients;  coefficients[5]x^5/coefficients[4]x^4/....
-    double c[ORDER];
-    // Takes in two arrays for points, the # of elements, and the order and stores it in the coefficients pointer
-    // return 0 -> we good
-    // return -1 -> no good
-    float midtime=MPI_Wtime()-starttime;
-    int result=polyfit(t_x,t_y,temp_max+temp_min,ORDER,c);
-    if (result==-1)
-    {
-        printf("OwO whoopsie\n");
-        return EXIT_FAILURE;
+        // Stores the coefficients;  coefficients[5]x^5/coefficients[4]x^4/....
+        double c[ORDER];
+        // Takes in two arrays for points, the # of elements, and the order and stores it in the coefficients pointer
+        // return 0 -> we good
+        // return -1 -> no good
+        float midtime=MPI_Wtime()-starttime;
+        int result=polyfit(t_x,t_y,temp_max+temp_min,ORDER,c);
+        if (result==-1)
+        {
+            printf("OwO whoopsie\n");
+            return EXIT_FAILURE;
+        }
+
+        print_equation(c);
+        // args - coefficients array and the X value it will use
+        // X value will be the "day" we are predicting for
+        // Will most likely need to add another arg for month
+        double prediction = predictTemp(c,100);
+        printf("%f\n",prediction);
     }
 
-    print_equation(c);
-    // args - coefficients array and the X value it will use
-    // X value will be the "day" we are predicting for
-    // Will most likely need to add another arg for month
-    double prediction = predictTemp(c,100);
-    printf("%f\n",prediction);
     float endtime=MPI_Wtime()-starttime;
     printf("Mid time:%f\n",midtime);
     printf("End time:%f\n",endtime);
